bd896fdda2ba2591da90b7f32980263c91d9b4ef
hita1999 <41143969+hita1999@users.noreply.github.com>
2023-04-03 01:57:37 +0900

    update: get title

--- soapcheck/soapcheck.go
+++ soapcheck/soapcheck.go
@@ -1,1 +1,20 @@
-package 
+package main
+
+import (
+	"fmt"
+
+	"github.com/gocolly/colly/v2"
+)
+
+func main() {
+	//targetURL
+	url := "https://www.uec-programming.com/e-learning/irohaboard/"
+
+	c := colly.NewCollector()
+
+	c.OnHTML("title", func(e *colly.HTMLElement) {
+		fmt.Println(e.Text)
+	})
+
+	c.Visit(url)
+}
