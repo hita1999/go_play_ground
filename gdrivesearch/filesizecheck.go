22fe1f1441fc4b5c75f4907f06b4168306700029
hita1999 <41143969+hita1999@users.noreply.github.com>
2023-04-07 17:01:14 +0900

    update: main()にてファイルサイズ表示の関数呼び出しを削除

--- gdrivesearch/filesizecheck.go
+++ gdrivesearch/filesizecheck.go
@@ -5,27 +5,13 @@ import (
 	"encoding/json"
 	"fmt"
 	"log"
-	"net/http"
 	"os"
 
+	"github.com/joho/godotenv"
 	"golang.org/x/oauth2"
-	"golang.org/x/oauth2/google"
 	"google.golang.org/api/drive/v3"
 	"google.golang.org/api/option"
 )
-
-type Credentials struct {
-	Type 	   string `json:"type"`
-	ProjectID  string `json:"project_id"`
-	PrivateKeyID string `json:"private_key_id"`
-	PrivateKey string `json:"private_key"`
-	ClientEmail string `json:"client_email"`
-	ClientID string `json:client_id"`
-	AuthURI string `json:"auth_uri"`
-	TokenURI string `json:"token_uri"`
-	AuthProviderX509 string `json:"auth_provider_x509_cert_url"`
-	ClientX509CertURL string `json:"client_x509_cert_url"`
-}
 
 func getTokenFromWeb(config *oauth2.Config) *oauth2.Token {
 	//OAtuh2.0の認証をリクエスト
@@ -43,15 +29,6 @@ func getTokenFromWeb(config *oauth2.Config) *oauth2.Token {
 		log.Fatalf("Unable to retrieve token from web: %v", err)
 	}
 	return token
-}
-
-func getClient(ctx context.Context, config *oauth2.Config) *http.Client {
-	tok, err := tokenFromFile(tokenFile)
-	if err != nil {
-		tok = getTokenFromWeb(config)
-		saveToken(tok)
-	}
-	return config.Client(ctx, tok)
 }
 
 func tokenFromFile(file string) (*oauth2.Token, error) {
@@ -76,14 +53,28 @@ func saveToken(file string, token *oauth2.Token) {
 	json.NewEncoder(f).Encode(token)
 }
 
-func printFiles(srv *drive.Service, query string) {
-	//Google Drive APIを初期化
-	if srv == nil {
-		log.Fatal("Unable to retrieve Drive client.")
+func main() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatalf("Error loading .env file: %v", err)
 	}
 
-	//ファイルの一覧を取得
-	r, err := srv.Files.List().Q(query).PageSize(1000).Fields("nextPageToken, files(id, name, size)").Do()
+	apiKey := os.Getenv("API_KEY")
+	accessToken := os.Getenv("ACCESS_TOKEN")
+	refreshToken := os.Getenv("REFRESH_TOKEN")
+
+	ctx := context.Background()
+	ts := oauth2.StaticTokenSource(
+		&oauth2.Token{AccessToken: accessToken, RefreshToken: refreshToken},
+	)
+	tc := oauth2.NewClient(ctx, ts)
+
+	srv, err := drive.NewService(ctx, option.WithAPIKey(apiKey), option.WithHTTPClient(tc))
+	if err != nil {
+		log.Fatalf("Unable to create Drive service: %v", err)
+	}
+
+	r, err := srv.Files.List().PageSize(1000).Fields("nextPageToken, files(id, name, size)").Do()
 	if err != nil {
 		log.Fatalf("Unable to retrieve files: %v", err)
 	}
@@ -95,36 +86,5 @@ func printFiles(srv *drive.Service, query string) {
 		for _, f := range r.Files {
 			fmt.Printf("%s (%s): %d bytes\n", f.Name, f.Id, f.Size)
 		}
-	}
-}
-
-func main() {
-	creds := &Credentials{}
-	err := json.Unmarshal([]byte(os.Getenv("GOOGLE_APPLICATION_CREDENTIALS_JSON")), creds)
-	if err != nil {
-		log.Fatalf("failed to unmarshal credentials: %v", err)
-	}
-
-	//Google Oauth2.0認証を使用してトークンを取得
-	ctx := context.Background()
-	config, err := google.ConfigFromJSON([]byte(os.Getenv("GOOGLE_APPLICATION_CREDENTIALS_JSON")), drive.DriveMetadataReadonlyScope)
-	if err != nil {
-		log.Fatalf("failed to create OAuth2 config: %v", err)
-	}
-	client := getClient(ctx, config)
-	srv, err := drive.NewService(ctx, option.WithHTTPClient(client))
-	if err != nil {
-		log.Fatalf("Unable to retrieve Drive client: %v", err)
-	}
-
-	//ファイル一覧を取得
-	files, err := srv.Files.List().Fields("nextPageToken, files(id, name, size)").Do()
-	if err != nil {
-		log.Fatalf("Unable to retrieve files: %v", err)
-	}
-
-	//ファイル情報を表示
-	for _, file := range files.Files {
-		fmt.Printf("File name: %s, ID: %s, Size: %d bytes\n", file.Name, file.Id, file.Size)
 	}
 }
--- gdrivesearch/go.mod
+++ gdrivesearch/go.mod
@@ -15,6 +15,7 @@ require (
 	github.com/google/uuid v1.3.0 // indirect
 	github.com/googleapis/enterprise-certificate-proxy v0.2.3 // indirect
 	github.com/googleapis/gax-go/v2 v2.8.0 // indirect
+	github.com/joho/godotenv v1.5.1
 	go.opencensus.io v0.24.0 // indirect
 	golang.org/x/net v0.9.0 // indirect
 	golang.org/x/sys v0.7.0 // indirect
